97250a73f00e058c22e1898ca256ebb696f8b581
root <root@localhost.localdomain>
2020-01-10 13:56:02 -0500

    update.sh

---
+++ update.sh
@@ -0,0 +1,46 @@
+
+clear
+
+sudo chmod +x /etc/
+
+clear
+
+sudo chmod +x /usr/share/doc
+
+clear
+
+sudo rm -rf /usr/share/doc/Black-B/
+
+clear
+
+cd /etc/
+
+clear
+
+sudo rm -rf /etc/Manisso
+
+clear
+
+mkdir Manisso
+
+clear
+
+cd Manisso
+
+clear
+
+git clone https://github.com/bensaad2/Black-B.git
+
+clear
+
+cd Black-B
+
+clear
+
+sudo chmod +x install.sh
+
+clear
+
+./install.sh
+
+clear
